33ddf9c59f8909ac964bb4cb90b3d9f2644d3cb9
haithem <haithemharaga@yahoo.co.uk>
2025-02-06 15:30:08 +0000

    set up connection code

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/products')
+      .then(response => setProducts(response.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image} alt={product.name} width="100" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
